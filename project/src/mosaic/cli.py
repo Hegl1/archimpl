7af3c3cfee72db7f4778b2d750c05ecce4542aa1
Marcel Alexander Huber <Marcel.Huber@student.uibk.ac.at>
2022-03-18 14:52:53 +0100

    feat(cli): added command line options

--- project/src/mosaic/cli.py
+++ project/src/mosaic/cli.py
@@ -1,12 +1,31 @@
 import click
+import os
+import sys
+
+
+def _load_tables_from_directory(directory):
+    for file in os.listdir(directory):
+        # TODO load tables from files in the given directory
+        click.echo(file)
+
+
+def _execute_query_file(file_path):
+    # TODO execute query files
+    click.echo(f"TODO: Execute query at: {file_path}")
+    sys.exit(0)
+
+
+def _main_loop():
+    # TODO create main loop of cli
+    pass
 
 
 @click.command()
-def main(args=None):
-    """Console script for mosaic."""
-    click.echo('Replace this message by putting your code into '
-               'mosaic.cli.main')
-    click.echo('See click documentation at http://click.pocoo.org/')
+@click.option("--data-directory", default="./", help="Directory which contains all tables to load at startup")
+@click.option("--query-file", default=None, help="Path to an optional query file to execute")
+def main(data_directory, query_file):
+    _load_tables_from_directory(data_directory)
+    if query_file is not None:
+        _execute_query_file(query_file)
     return 0
-
 
