b31c38ef0e14f59c73a9959b978e20e981d65861
Marcel Alexander Huber <Marcel.Huber@student.uibk.ac.at>
2022-03-18 15:46:03 +0100

    feat(cli): added query input and support for commands

--- project/src/mosaic/cli.py
+++ project/src/mosaic/cli.py
@@ -11,21 +11,77 @@ def _load_tables_from_directory(directory):
 
 def _execute_query_file(file_path):
     # TODO execute query files
+    # TODO handle invalid file paths
     click.echo(f"TODO: Execute query at: {file_path}")
+
+
+def _quit_application():
+    click.echo("Bye!")
     sys.exit(0)
 
 
+def _print_command_help():
+    click.echo("\\help \t\t\t\t shows this output.")
+    click.echo("\\execute <query-file> \t\t executes the query loaded from query-file.")
+    click.echo("\\quit \t\t\t\t quits the application.")
+    click.echo("\\clear \t\t\t\t clears the screen.")
+    click.echo("")
+    click.echo("<query> \t\t\t executes a query that needs to be terminated by \";\"")
+
+
+def _execute_query_file_from_command(user_in):
+    split_string = user_in.split(" ")
+    if len(split_string) != 2:
+        click.echo("Wrong usage of \\execute. See \\help for further detail")
+    else:
+        _execute_query_file(split_string[1])
+
+
+def _execute_command(user_in):
+    if user_in == "\\help":
+        _print_command_help()
+    elif user_in == "\\quit":
+        _quit_application()
+    elif user_in == "\\clear":
+        click.clear()
+    elif "\\execute" in user_in:
+        _execute_query_file_from_command(user_in)
+    else:
+        click.echo("Unknown command entered. See \\help for a list of available commands.")
+
+
+def _execute_query(user_in):
+    print("query: " + user_in)
+
+
+def _multi_line_loop(user_in):
+    while user_in[-1] != ';':
+        click.echo(">   ", nl=False)
+        user_in += input()
+    return user_in
+
+
 def _main_loop():
-    # TODO create main loop of cli
-    pass
+    while True:
+        click.echo(">>> ",nl=False)
+        user_in = input()
+        if user_in[0] == "\\":
+            _execute_command(user_in)
+        else:
+            if user_in[-1] != ';':
+                # Maybe add newlines between inputs, depending on the provided query parser
+                user_in = _multi_line_loop(user_in)
+            _execute_query(user_in)
 
 
 @click.command()
-@click.option("--data-directory", default="./", help="Directory which contains all tables to load at startup")
-@click.option("--query-file", default=None, help="Path to an optional query file to execute")
+@click.option("--data-directory", default="./", type=click.Path(exists=True),help="Directory which contains all tables to load at startup")
+@click.option("--query-file", default=None, type=click.Path(exists=True),help="Path to an optional query file to execute")
 def main(data_directory, query_file):
     _load_tables_from_directory(data_directory)
     if query_file is not None:
         _execute_query_file(query_file)
+        sys.exit(0)
+    _main_loop()
     return 0
 
