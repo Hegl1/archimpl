a306120e025b5a74e40da9a48ddfceb0363d2913
Matthias Thalmann <matthias.thalmann@student.uibk.ac.at>
2022-04-15 12:33:01 +0200

    feat(hash_distinct): implement basic hash-distinct expressions

--- project/src/mosaic/expressions/hash_distinct.py
+++ project/src/mosaic/expressions/hash_distinct.py
@@ -7,8 +7,24 @@ class HashDistinct(AbstractExpression):
         self.table = table
 
     def get_result(self):
-        # TODO: implement
-        return self.table.get_result()
+        table = self.table.get_result()
+
+        hashes = set()
+        duplicate_indices = []
+
+        for index, record in enumerate(table.records):
+            record_hash = hash(str(record))
+            if record_hash in hashes:
+                duplicate_indices.append(index)
+            else:
+                hashes.add(record_hash)
+
+        duplicate_indices.reverse()
+
+        for index in duplicate_indices:
+            table.records.pop(index)
+
+        return table
 
     def __str__(self):
         return f"HashDistinct(table_name={self.table.table_name})"
