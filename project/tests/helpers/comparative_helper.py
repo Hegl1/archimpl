347a020edc7cec9654fcb74e907fde1f90fcad4f
Aaron Targa <aaron.targa@student.uibk.ac.at>
2022-04-21 12:04:59 +0200

    tests(compiler): added tests for compartive operation expression

---
+++ project/tests/helpers/comparative_helper.py
@@ -0,0 +1,15 @@
+from mosaic import table_service
+
+def evaluate(comparative_operation):
+    
+    table = retrieve_table("studenten")
+    sum = 0
+    entries = len(table.records)
+    for i in range(entries):
+        sum += comparative_operation.get_result(table=table,row_index=i)
+
+    return (sum,entries)
+
+def retrieve_table(name):
+    table_service.load_tables_from_directory("./tests/testdata/")
+    return table_service.retrieve(name)
