cddad0a4885ea11fa112531e3e7e1ae0561c44e5
Thomas Klotz <thomas.klotz98@gmail.com>
2022-04-22 22:18:24 +0200

    tests(set_expressions): added test for set_expressions, expanded test_explain accordingly

--- project/tests/mosaic/expressions/test_explain.py
+++ project/tests/mosaic/expressions/test_explain.py
@@ -40,15 +40,33 @@ def test_explain_selection():
 
 
 def test_explain_union():
-    pass
+    result, _ = execute_query("explain pi VorlNr as Vorgaenger voraussetzen union pi VorlNr vorlesungen;")[0]
+    assert len(result.records) == 5
+    assert result.records[0][0] == "-->Union"
+    assert result.records[1][0] == "---->Projection(columns=[VorlNr=Vorgaenger])"
+    assert result.records[2][0] == "------>TableScan(voraussetzen)"
+    assert result.records[3][0] == "---->Projection(columns=[VorlNr=VorlNr])"
+    assert result.records[4][0] == "------>TableScan(vorlesungen)"
 
 
 def test_explain_intersect():
-    pass
+    result, _ = execute_query("explain pi VorlNr vorlesungen intersect pi VorlNr as Vorgaenger voraussetzen;")[0]
+    assert len(result.records) == 5
+    assert result.records[0][0] == "-->Intersect"
+    assert result.records[1][0] == "---->Projection(columns=[VorlNr=VorlNr])"
+    assert result.records[2][0] == "------>TableScan(vorlesungen)"
+    assert result.records[3][0] == "---->Projection(columns=[VorlNr=Vorgaenger])"
+    assert result.records[4][0] == "------>TableScan(voraussetzen)"
 
 
 def test_explain_difference():
-    pass
+    result, _ = execute_query("explain pi VorlNr as Vorgaenger voraussetzen except pi VorlNr vorlesungen;")[0]
+    assert len(result.records) == 5
+    assert result.records[0][0] == "-->Except"
+    assert result.records[1][0] == "---->Projection(columns=[VorlNr=Vorgaenger])"
+    assert result.records[2][0] == "------>TableScan(voraussetzen)"
+    assert result.records[3][0] == "---->Projection(columns=[VorlNr=VorlNr])"
+    assert result.records[4][0] == "------>TableScan(vorlesungen)"
 
 
 def test_explain_cross_join():
