2cf5ab1b1f580990ba03df3b8bb338d324330bda
Thomas Klotz <thomas.klotz98@gmail.com>
2022-04-23 18:32:22 +0200

    feat(selection): fix parentheses for string method

--- project/src/mosaic/expressions/selection.py
+++ project/src/mosaic/expressions/selection.py
@@ -20,7 +20,7 @@ class Selection(AbstractExpression):
         return Table(table.table_name, table.schema_names, table.schema_types, result)
 
     def __str__(self):
-        return f"Selection(condition=({self.condition.__str__()}))"
+        return f"Selection(condition={self.condition.__str__()})"
 
     def explain(self, rows, indent):
         rows.append([indent * "-" + ">" + self.__str__()])
