47d9bf94222350e385ad133d063eb46e139bebfe
Thomas Klotz <thomas.klotz98@gmail.com>
2022-05-07 16:57:13 +0200

    refactor(compiler): removed unnecessary methods in abstract classes

--- project/src/mosaic/compiler/abstract_compile_node.py
+++ project/src/mosaic/compiler/abstract_compile_node.py
@@ -3,8 +3,6 @@ from abc import abstractmethod
 
 
 class AbstractCompileNode(ABC):
-    def __init__(self):
-        pass
 
     @abstractmethod
     def __str__(self): # pragma: no cover
@@ -12,16 +10,3 @@ class AbstractCompileNode(ABC):
         Returns the string used to explain the execution plan for this command.
         """
         pass
-
-    def explain(self, rows, indent):
-        """
-        Method to build a list of strings for the explain command.
-        Adds the representative String of the current command
-        in the list of rows and then calls this method on child nodes.
-        The representative string needs to be correctly indented and wrapped in a list.
-
-        Args:
-            rows: list to add the representative string to.
-            indent: Describes the level of indentation at the current command.
-        """
-        rows.append([indent * "-" + ">" + self.__str__()])
--- project/src/mosaic/compiler/expressions/abstract_expression.py
+++ project/src/mosaic/compiler/expressions/abstract_expression.py
@@ -21,16 +21,3 @@ class AbstractExpression(AbstractCompileNode):
         Returns the string used to explain the execution plan for this command.
         """
         pass
-
-    def explain(self, rows, indent):
-        """
-        Method to build a list of strings for the explain command.
-        Adds the representative String of the current command
-        in the list of rows and then calls this method on child nodes.
-        The representative string needs to be correctly indented and wrapped in a list.
-
-        Args:
-            rows: list to add the representative string to.
-            indent: Describes the level of indentation at the current command.
-        """
-        rows.append([indent * "-" + ">" + self.__str__()])
