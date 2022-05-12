cfd26ed0d28814633c233b3f886cf82e5cc335ac
Marcel Alexander Huber <Marcel.Huber@student.uibk.ac.at>
2022-05-12 13:08:25 +0200

    refactor(join): extracted get_schema mechanism from get result, restructured join methods

--- project/src/mosaic/compiler/operators/abstract_join_operator.py
+++ project/src/mosaic/compiler/operators/abstract_join_operator.py
@@ -5,7 +5,9 @@ from .abstract_operator import AbstractOperator
 
 
 class JoinType(Enum):
-    CROSS = "cross_join"
+    CROSS = "cross"
+    INNER = "inner"
+    LEFT_OUTER = "left_outer"
     # TODO add more join types as needed
 
 
@@ -27,11 +29,38 @@ class AbstractJoin(AbstractOperator):
     def get_schema(self):
         schema1 = self.table1_reference.get_schema()
         schema2 = self.table2_reference.get_schema()
-        check_table_names(schema1, schema2)
-        check_condition(schema1, schema2, self.condition)
-        joined_table_name = f"{schema1.table_name}_cross_join_{schema2.table_name}"
-        return Schema(joined_table_name, schema1.column_names + schema2.column_names,
-                      schema1.column_types + schema2.column_types)
+        return self._build_schema(schema1, schema2)
+
+    def _build_schema(self, schema1, schema2):
+
+        self.check_table_names(schema1, schema2)
+        if self.is_natural and self.join_type != JoinType.CROSS:
+            return self._get_natural_join_schema(schema1, schema2)
+        else:
+            self.check_condition(schema1, schema2, self.condition)
+            joined_table_name = f"{schema1.table_name}_{self.join_type}_join_{schema2.table_name}"
+            return Schema(joined_table_name, schema1.column_names + schema2.column_names,
+                          schema1.column_types + schema2.column_types)
+
+    def _get_natural_join_schema(self, schema1, schema2):
+        joined_table_name = f"{schema1.table_name}_natural_{self.join_type}_join_{schema2.table_name}"
+        schema2_col_names = []
+        schema2_col_types = []
+        for schema_name, schema_type in zip(schema2.column_names, schema2.column_types):
+            if schema2.get_simple_column_name(schema_name) not in schema1.get_simple_column_name_list():
+                schema2_col_names.append(schema_name)
+                schema2_col_types.append(schema_type)
+        return Schema(joined_table_name, schema1.column_names + schema2_col_names,
+                      schema1.column_types + schema2_col_types)
+
+    @abstractmethod
+    def check_condition(self, schema1, schema2, condition):
+        pass
+
+    def check_table_names(self, schema1, schema2):
+        if schema1.table_name == schema2.table_name:
+            raise SelfJoinWithoutRenamingException(f"Table \"{schema1.table_name}\" can't be joined with itself "
+                                                   f"without renaming one of the occurrences")
 
     def explain(self, rows, indent):
         super().explain(rows, indent)
@@ -39,18 +68,6 @@ class AbstractJoin(AbstractOperator):
         self.table2_reference.explain(rows, indent + 2)
 
 
-def check_table_names(schema1, schema2):
-    if schema1.table_name == schema2.table_name:
-        raise SelfJoinWithoutRenamingException(f"Table \"{schema1.table_name}\" can't be joined with itself "
-                                               f"without renaming one of the occurrences")
-
-
-def check_condition(schema1, schema2, condition):
-    if condition is None:
-        return
-    # TODO implement together with other join types
-    # if we don't compare something with columns from both different tables, raise ConditionNotValidException
-    pass
 
 
 class ConditionNotValidException(Exception):
--- project/src/mosaic/compiler/operators/nested_loops_join_operators.py
+++ project/src/mosaic/compiler/operators/nested_loops_join_operators.py
@@ -21,17 +21,15 @@ class NestedLoopsJoin(AbstractJoin):
         table1 = self.table1_reference.get_result()
         table2 = self.table2_reference.get_result()
 
-        check_table_names(table1.schema, table2.schema)
-        check_condition(table1.schema, table2.schema, self.condition)
+        self.check_table_names(table1.schema, table2.schema)
+        self.check_condition(table1.schema, table2.schema, self.condition)
 
         if self.join_type == JoinType.CROSS:
             joined_table_records = []
             for record1 in table1.records:
                 for record2 in table2.records:
                     joined_table_records.append(record1 + record2)
-            joined_table_name = f"{table1.table_name}_cross_join_{table2.table_name}"
-            schema = Schema(joined_table_name, table1.schema.column_names + table2.schema.column_names,
-                            table1.schema.column_types + table2.schema.column_types)
+            schema = self._build_schema(table1.schema, table2.schema)
 
             return Table(schema, joined_table_records)
         # TODO handle other join types correctly
@@ -40,3 +38,10 @@ class NestedLoopsJoin(AbstractJoin):
     def __str__(self):
         return f"NestedLoopsJoin(cross, natural={self.is_natural}, condition={self.condition})"
         # TODO does the condition already have to contain only fqn? yes?
+
+    def check_condition(self, schema1, schema2, condition):
+        if condition is None:
+            return
+        # TODO implement together with other join types
+        # if we don't compare something with columns from both different tables, raise ConditionNotValidException
+        pass
